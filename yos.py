a93921235a8d7c9a8ba4c023e7be3b5a3f1ecb85
Ritchie Smith <dasnarrenschiff@gmail.com>
2019-01-09 16:52:38 +0000

    Added Q. Thanks Captain Foo.

--- yos.py
+++ yos.py
@@ -27,6 +27,7 @@ emojis = {" " : [":sonicrun1:", ":sonicrun2:", ":sonicrun3:", ":sonicrun4:", ":s
 "O" : [":0:", ":amiga:", ":awesome:", ":bloomin:", ":nhl-wpg:"],
 "OO" : [":lemniscate:"],
 "OWO" : [":owo:"],
+"Q" : [":quicktime:"],
 "R" : [":r:", ":buffering:", ":rainier:"],
 "S" : [":s:"],
 "SN" : [":gettin:"],
